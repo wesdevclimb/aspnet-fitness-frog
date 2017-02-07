d930619d832cda11ab5bbd09a01119f80d11cfb2
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-07 14:02:47 -0500

    Yo

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
+
 using System.Web.Mvc;
 using Treehouse.FitnessFrog.Data;
 using Treehouse.FitnessFrog.Models;
