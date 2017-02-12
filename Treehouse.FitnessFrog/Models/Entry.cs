87027317439dbefab105e5f928e6909de7d6883c
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-12 14:28:02 -0600

    Added validation rules using data annotations

--- Treehouse.FitnessFrog/Models/Entry.cs
+++ Treehouse.FitnessFrog/Models/Entry.cs
@@ -89,6 +89,7 @@ namespace Treehouse.FitnessFrog.Models
         /// <summary>
         /// The notes for the entry.
         /// </summary>
+        [MaxLength(200, ErrorMessage = "Notes cannot be longer than 200 characters")]
         public string Notes { get; set; }
     }
 }
